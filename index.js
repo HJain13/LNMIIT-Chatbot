4814325a1ca63c3a0cb40a67fd900f89a2fc743b
Himesh Jain <himeshjain13@gmail.com>
2018-04-20 02:50:24 +0530

    Add: Content Processing through fs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const api = require('./api/routes');
+const fs = require('fs');
 
 // Initialising App
 var app = express();
@@ -18,8 +19,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Creating the Database
+function getPrecedingValue(match){
+	return ' ' + match[match.length - 1];
+}
 
+fs.readFile('./data/content.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Reading Content from Data Folder.');        
+        const content = data;
+        var processedContent = content.replace(/\[\d\]/g, '').replace(/\. [a-z]/g,getPrecedingValue).replace(/[\n\r]/g, ' ');
+        var sentences = processedContent.split('. ');
+        console.log('Processing!');        
+        fs.writeFile('./data/processed.txt', JSON.stringify(sentences), 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Processed content was Saved!');
+            }
+        });
+    }
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
 
 app.use(function(err, req, res, next) {
