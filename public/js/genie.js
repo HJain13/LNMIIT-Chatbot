db337327768a147e9abb37f1c7c0db776b2630a9
Himesh Jain <himeshjain13@gmail.com>
2018-05-15 10:50:33 +0530

    First Part Code Complete

--- public/js/genie.js
+++ public/js/genie.js
@@ -11,6 +11,7 @@ function genie(){
 			var bestAnswer = response['best_answer']['answer'];
 			document.getElementById('chat').innerHTML = document.getElementById('chat').innerHTML + '<div class="columns is-marginless"><div class="column is-1 has-no-right-padding"><img src="http://www.askanthonyjohnson.com/wp-content/uploads/2017/11/bot-icon-2883144_640-300x300.png" alt=""></div><div class="column is-11"><div class="is-chat-bubble-bot">'+bestAnswer+'</div></div>';
 			window.scroll(0, document.getElementById('chat').scrollHeight);
+			document.getElementById('query').value = '';
 		}
 	};
 	xhttp.open("GET", "/api/answer/"+encodeURI(query), true);
